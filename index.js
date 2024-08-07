01d67ec037cab123b80c0b3cab2fdccb73f0aca6
notbladeuwu@gmail.com <notbladeuwu@gmail.com>
2024-08-07 17:28:11 +0530

    "Added ejsMate engine and refactored index.ejs to use boilerplate layout"

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
 
 main().then((res)=>{
     console.log("Connected Database !!!");
